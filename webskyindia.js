5838e10e07b32aaea1c41ae202454ecdff240370
Rahul Sharma <53206826+RahulSharma1997@users.noreply.github.com>
2021-05-22 15:08:16 +0530

    Delete webskyindia.js

--- webskyindia.js
+++
@@ -1,2 +0,0 @@
-var websky = `<a href="http://www.webskyindia.com/"><img src="https://raw.githubusercontent.com/RahulSharma1997/webskyindia/main/logo/websky.png"></a>`;
-$('#webskyindia').append(websky);
