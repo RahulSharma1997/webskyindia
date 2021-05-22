1f84fb91f3a153946254460a9faba96406f2075c
Rahul Sharma <53206826+RahulSharma1997@users.noreply.github.com>
2021-05-22 15:54:49 +0530

    Update webskyindia.js

--- webskyindia.js
+++ webskyindia.js
@@ -1,2 +1,2 @@
-var websky = `<a href="http://www.webskyindia.com/" target="_blank"><img src="https://raw.githubusercontent.com/RahulSharma1997/webskyindia/main/logo/websky.png"></a>`;
+var websky = `<a href="http://www.webskyindia.com/" target="_blank"><img src="https://raw.githubusercontent.com/RahulSharma1997/webskyindia/main/logo/websky_india.png"></a>`;
 $('#webskyindia').append(websky);
